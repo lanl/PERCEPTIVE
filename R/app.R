c5e595f3ee762315b2a58b2d1014613e2b93b2bb
Jon Sykora <jsykora@unm.edu>
2025-05-06 17:34:45 -0600

    cont

--- R/app.R
+++ R/app.R
@@ -1392,7 +1392,7 @@ observeEvent(input$example,
                 showModal(modalDialog(
                  title = "Example",
                  #HTML(paste0('<img src="',find.package("PERCEPTIVEv3"),'/schema.jpg"/>')),
-                 tag$img(src="https://github.com/lanl/PERCEPTIVE/blob/main/inst/schema.jpg"),
+                 tags$img(src = "https://github.com/lanl/PERCEPTIVE/blob/main/inst/schema.jpg" ),
                  easyClose = TRUE,
                  footer = NULL
              ))}
